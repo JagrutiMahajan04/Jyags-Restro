194c51d32482b36030f27b9ed744d0f8da60bb4e
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2023-01-10 16:39:37 +0530

    Tables Created

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 import User from "./models/User.js";
 import FoodItem from "./models/FoodItem.js";
+import Table from "./models/Table.js";
 
 const app = express();
 app.use(express.json());
@@ -60,15 +61,6 @@ app.post('/signup',async(req, res)=>{
    }
  //validation  to check if phone already exists ends here
 
-
-
-
-
-
-
-
-
-
     const user = new User({
         name: name,
         phone: phone,
@@ -114,10 +106,10 @@ app.post('/login', async (req, res)=> {
     }
 })
 
-app.post('/createfoodItem', async(req,res)=>{
+app.post('/createFoodItem', async(req,res)=>{
     const {title, description, imgUrl, price, category} = req.body;
 
-    const foodItemSchema = new Schema({
+    const foodItem = new FoodItem({
         title: title,
         description:description,
         imgUrl: imgUrl,
@@ -125,7 +117,7 @@ app.post('/createfoodItem', async(req,res)=>{
         category:category
     })
 
-    const savedFoodItem = await foodItemSchema.save();
+    const savedFoodItem = await foodItem.save();
 
     res.json({
         success: true,
@@ -161,6 +153,84 @@ app.get("/foodItems", async(req, res)=>{
         success: true,
         message:"Food Items fetched successfully",
         data: foodItems
+    })
+})
+
+app.post("/createTable", async(req,res)=>{
+    const {tableNumber} = req.body;
+
+    const existingTable = await Table.findOne({tableNumber: tableNumber});
+    if (existingTable){
+        return res.json({
+            success:false,
+            message:"Table already exists"
+        })
+    }
+
+    const table = new Table({
+        tableNumber: tableNumber,
+        booked:false
+    })
+
+    const savedTable = await table.save();
+
+    res.json({
+        success:true,
+        message:"Table created successfully",
+        data:savedTable
+    })
+})
+
+app.post("/bookTable", async (req, res)=>{
+    const  { tableNumber, userId } = req.body;
+
+    const existingTable = await Table.findOne({ tableNumber: tableNumber});
+    if (existingTable && existingTable.booked) {
+        return res.json({
+            success: false,
+            message: "Table already booked."
+        })
+    }
+
+    if(existingTable){
+        existingTable.booked = true;
+        existingTable.bookedBy = userId;
+        await existingTable.save();
+    }
+
+    res.json({
+        success:true,
+        message:"Table booked successfully",
+        data:existingTable
+    })
+})
+
+app.post("/unbookTable", async (req, res)=> {
+    const {tableNumber} = req.body;
+
+    const existingTable = await Table.findOne({ tableNumber: tableNumber });
+
+    if(existingTable){
+        existingTable.booked = false;
+        existingTable.bookedBy = null;
+        await existingTable.save();
+    }
+
+    res.json({
+        success:true,
+        message:"Table unbooked successfully",
+        data:existingTable
+    })
+});
+
+app.get("/availableTables", async (req, res)=> {
+    const availableTables = await Table.find({ booked : false });
+
+    res.json({
+
+        success:true,
+        message:"Available tables fetched successfully",
+        data: availableTables
     })
 })
 
