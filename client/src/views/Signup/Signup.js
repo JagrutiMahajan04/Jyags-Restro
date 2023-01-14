65743262e19f23104c9eae0a0317c9b823904d0b
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2023-01-14 16:55:30 +0530

    sweetAlert done

--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,44 +1,56 @@
-import React, {useState,useEffect} from "react";
-import { currentUser } from "../../util/CurrentUser";
+import React, { useState, useEffect } from "react";
+import swal from 'sweetalert'
 import axios from 'axios'
+
+import { currentUser } from "../../util/CurrentUser";
 import "./Signup.css"
 
 function Signup() {
-    const [name,setName] = useState('')
-    const [email,setEmail] = useState('')
-    const [phone,setPhone] = useState('')
-    const [password,setPassword] = useState('')
-    const [role,setRole] = useState('user')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [password, setPassword] = useState('')
+    const [role, setRole] = useState('user')
 
 
     useEffect(() => {
-        if(currentUser){
-         window.location.href="/"
+        if (currentUser) {
+            window.location.href = "/"
         }
     }, [])
 
-   async function signupUser() {
-    const response = await axios.post('/signup', {
-        name:name,
-        email:email,
-        phone:phone,
-        password:password,
-        role:role
-    })
-    console.log(response.data)
-    if(response.data.success){
-        alert(response.data.message)
-        window.location.href = '/login'
-    }
-     else{
-        alert(response.data.message)
-        setName('')
-        setEmail('')
-        setPhone('')
-        setPassword('')
-     }
+    async function signupUser() {
+        const response = await axios.post('/signup', {
+            name: name,
+            email: email,
+            phone: phone,
+            password: password,
+            role: role
+        })
+        console.log(response.data)
+        if (response.data.success) {
+           await swal({
+                title: "Success",
+                text: response.data.message,
+                icon: "success",
+                button: "Aww yiss!",
+            });
+            window.location.href = '/login'
+        }
+        else {
+           await swal({
+                title: "Error",
+                text: response.data.message,
+                icon: "error",
+                button: "Try Again",
+            });
+            setName('')
+            setEmail('')
+            setPhone('')
+            setPassword('')
+        }
 
-   }
+    }
 
 
     return (
@@ -52,32 +64,32 @@ function Signup() {
                 <div className="col-md-6">
                     <div className="form-container">
                         <form>
-                            <div > 
-                            <label htmlFor="name"> Full Name:</label>
-                            <input type='text' id="name"placeholder="Enter Name" 
-                             className='user-input'
-                             value={name} onChange={(e) => setName(e.target.value)}/>
+                            <div >
+                                <label htmlFor="name"> Full Name:</label>
+                                <input type='text' id="name" placeholder="Enter Name"
+                                    className='user-input'
+                                    value={name} onChange={(e) => setName(e.target.value)} />
                             </div>
 
-                            <div > 
-                            <label htmlFor="email"> Email:</label>
-                            <input type='email' id="email"placeholder="Enter Email" 
-                             className='user-input'
-                             value={email} onChange={(e) => setEmail(e.target.value)}/>
+                            <div >
+                                <label htmlFor="email"> Email:</label>
+                                <input type='email' id="email" placeholder="Enter Email"
+                                    className='user-input'
+                                    value={email} onChange={(e) => setEmail(e.target.value)} />
                             </div>
 
-                            <div > 
-                            <label htmlFor="phone"> Phone:</label>
-                            <input type='text' id="phone"placeholder="Enter Phone" 
-                             className='user-input'
-                             value={phone} onChange={(e) => setPhone(e.target.value)}/>
+                            <div >
+                                <label htmlFor="phone"> Phone:</label>
+                                <input type='text' id="phone" placeholder="Enter Phone"
+                                    className='user-input'
+                                    value={phone} onChange={(e) => setPhone(e.target.value)} />
                             </div>
 
-                            <div > 
-                            <label htmlFor="password"> Password:</label>
-                            <input type='password' id="password"placeholder="Enter password" 
-                             className='user-input'
-                             value={password} onChange={(e) => setPassword(e.target.value)}/>
+                            <div >
+                                <label htmlFor="password"> Password:</label>
+                                <input type='password' id="password" placeholder="Enter password"
+                                    className='user-input'
+                                    value={password} onChange={(e) => setPassword(e.target.value)} />
                             </div>
 
                             <div>
